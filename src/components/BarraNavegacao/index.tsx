847f82be4a53c27f1c3e8fac2a4932a973bbf8b8
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-05 20:48:43 -0300

    :sparkles: feat: Adiciona acesso para a conta do usuário
    Adiciona login e cadastro do usuário na barra de navegação

--- src/components/BarraNavegacao/index.tsx
+++ src/components/BarraNavegacao/index.tsx
@@ -1,8 +1,32 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import ModalLoginUsuario from '@/components/ModalLoginUsuario'
+import ModalCadastroUsuario from '@/components/ModalCadastroUsuario'
+import ContaNavegacao from './ContaNavegacao'
 import logo from './assets/logo.png'
-import styles from './BarraNavegação.module.scss'
+import usuario from './assets/usuario.svg'
+import styles from './BarraNavegacao.module.scss'
 
 const BarraNavegacao = () => {
+    const [modalLoginAberta, setModalLoginAberta] = useState<boolean>(false)
+    const [modalCadastroAberta, setModalCadastroAberta] = useState<boolean>(false)
+
+    const navigate = useNavigate()
+    const token = sessionStorage.getItem('token')
+
+    const [usuarioEstaLogado, setUsuarioEstaLogado] = useState<boolean>(token !== null)
+
+    const aoEfetuarLogin = () => {
+        setModalLoginAberta(false)
+        setUsuarioEstaLogado(true)
+    }
+
+    const efetuarLogout = () => {
+        setUsuarioEstaLogado(false)
+        sessionStorage.removeItem('token')
+        navigate('/')
+    }
+
     return (
         <nav className={styles.navbar}>
             <h1>
@@ -40,6 +64,54 @@ const BarraNavegacao = () => {
                     </li>
                 </ul>
             </div>
+            <ul className={styles.usuario}>
+                {!usuarioEstaLogado && (
+                    <>
+                        <li>
+                            <ContaNavegacao
+                                texto="Login"
+                                imagem={usuario}
+                                imagemAlt="Icone representando um usuário"
+                                onClick={() => setModalLoginAberta(true)}
+                            />
+                            <ModalLoginUsuario
+                                aberta={modalLoginAberta}
+                                aoFechar={() => setModalLoginAberta(false)}
+                                aoEfetuarLogin={aoEfetuarLogin}
+                            />
+                        </li>
+                        <li>
+                            <ContaNavegacao
+                                texto="Cadastrar-se"
+                                imagem={usuario}
+                                imagemAlt="Icone representando um usuário"
+                                onClick={() => setModalCadastroAberta(true)}
+                            />
+                            <ModalCadastroUsuario
+                                aberta={modalCadastroAberta}
+                                aoFechar={() => setModalCadastroAberta(false)}
+                            />
+                        </li>
+                    </>
+                )}
+                {usuarioEstaLogado && (
+                    <>
+                        <li>
+                            <Link className={styles.conta} to="/minha-conta/pedidos">
+                                Minha conta
+                            </Link>
+                        </li>
+                        <li>
+                            <ContaNavegacao
+                                texto="Logout"
+                                imagem={usuario}
+                                imagemAlt="Icone representando um usuário"
+                                onClick={efetuarLogout}
+                            />
+                        </li>
+                    </>
+                )}
+            </ul>
         </nav>
     )
 }
