e6026f481af25ff604be8981525a64010c9ddb34
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-06 22:54:59 -0300

    :sparkles: feat: Adiciona mais rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,18 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Layout from '@/pages/Layout'
+import Home from '@/pages/Home'
+import AreaLogada from '@/pages/AreaLogada'
+import Pedidos from '@/pages/Pedidos'
 
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Layout />}>
-                    
+                    <Route index element={<Home />} />
+                    <Route path="minha-conta" element={<AreaLogada />}>
+                        <Route path="pedidos" element={<Pedidos />} />
+                    </Route>
                 </Route>
             </Routes>
         </Router>
